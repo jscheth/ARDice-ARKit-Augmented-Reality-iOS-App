f9e1e432eb7e6d1289b58ae6787d6e6a89520f9b
jscheth <sovichea.chet@gmail.com>
2025-05-15 13:56:27 -0700

    Initial Commit

---
+++ ARDiceTests/ARDiceTests.swift
@@ -0,0 +1,17 @@
+//
+//  ARDiceTests.swift
+//  ARDiceTests
+//
+//  Created by Jonathan Cheth on 5/15/25.
+//
+
+import Testing
+@testable import ARDice
+
+struct ARDiceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
