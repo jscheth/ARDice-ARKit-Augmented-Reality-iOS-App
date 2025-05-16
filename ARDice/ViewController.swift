22229152ac8b013c7dd77246e3427d50bca52b6e
jscheth <sovichea.chet@gmail.com>
2025-05-16 14:53:32 -0700

    Play around with Cube and Moon AR

--- ARDice/ViewController.swift
+++ ARDice/ViewController.swift
@@ -19,14 +19,50 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        // Cube
+//        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+//        
+//        let material = SCNMaterial()
+//        
+//        material.diffuse.contents = UIColor.red
+//        
+//        cube.materials = [material]
+//        
+//        let node = SCNNode()
+//        
+//        node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
+//        
+//        node.geometry = cube
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        // Moon
+//        let sphere = SCNSphere(radius: 0.2)
+//        
+//        let material = SCNMaterial()
+//        
+//        material.diffuse.contents = UIImage(named: "art.scnassets/8k_moon.jpg")
+//        
+//        sphere.materials = [material]
+//        
+//        let node = SCNNode()
+//        
+//        node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
+//        
+//        node.geometry = sphere
+//        
+//        sceneView.scene.rootNode.addChildNode(node)
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        sceneView.autoenablesDefaultLighting = true
+        
+        
+        
+//        // Show statistics such as fps and timing information
+//        sceneView.showsStatistics = true
+//        
+//        // Create a new scene
+//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+//        
+//        // Set the scene to the view
+//        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
